9b6476fc0acd8045c36c38702af5692df2353f79
Martin Littlecott <mlittlec@hotmail.com>
2022-12-20 16:52:42 +0000

    Add Dark Mode toggle to the App

--- StatsApp.py
+++ StatsApp.py
@@ -1,11 +1,21 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Header
+from textual.widgets import Header, Footer
 
 
 class StatsApp(App):
+    """" A template Textual application to process our Usage Stats"""
     # pass
+
+    BINDINGS = [("d", "toggle_dark", "Toggle Dark Mode")]
+
     def compose(self) -> ComposeResult:
+        """Create Child widgets for the app."""
         yield Header()
+        yield Footer()
+
+    def action_toggle_dark(self) -> None:
+        """An action to toggle daerk mode"""
+        self.dark = not.self.dark
 
 
 if __name__ == "__main__":
