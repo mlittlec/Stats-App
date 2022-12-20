54f9d6e539c1684310c9c174a492504c4ce1b44b
Martin Littlecott <mlittlec@hotmail.com>
2022-12-20 22:54:44 +0000

    Correct typo and add Quit option

--- StatsApp.py
+++ StatsApp.py
@@ -1,4 +1,5 @@
 from textual.app import App, ComposeResult
+from textual.binding import Binding
 from textual.widgets import Header, Footer
 
 
@@ -6,7 +7,7 @@ class StatsApp(App):
     """" A template Textual application to process our Usage Stats"""
     # pass
 
-    BINDINGS = [("d", "toggle_dark", "Toggle Dark Mode")]
+    BINDINGS = [("d", "toggle_dark", "Toggle Dark Mode"), Binding("ctrl+c,ctrl+q", "app.quit", "Quit", show=True)]
 
     def compose(self) -> ComposeResult:
         """Create Child widgets for the app."""
@@ -14,7 +15,7 @@ class StatsApp(App):
         yield Footer()
 
     def action_toggle_dark(self) -> None:
-        """An action to toggle daerk mode"""
+        """An action to toggle dark mode"""
         self.dark = not self.dark
 
 
